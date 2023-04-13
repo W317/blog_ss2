f367d2c0115c86c710c1b8ade78a15794a24cc17
huyenthanhan13 <125731667+huyenthanhan13@users.noreply.github.com>
2023-04-13 16:42:32 +0700

    change notice cart

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
 import blogRouter from "./blogRoute.js";
-import adminRoute from "./AdminProductRoute.js";
+import adminRoute from "./AdminRoute.js";
 import userRouter from "./userRoute.js";
 import cartRoute from "./cartRoute.js";
 import asyncHandler from "express-async-handler";
 import BlogSchema from "../app/models/blogModel.js";
 import authRoute from './AuthRoute.js'
 import productRoute from "./ProductRoute.js"
-import userRoute from "./AdminUserRoute.js"
+import userRoute from "./userRoute.js"
+import orderRoute from "./OrderRoute.js"
 import path from "path";
 
 export default function route(app) {
